62087b3e4a7ceecfef1f3cd23a9f511faee57124
inno-cent <ekweozorinnocent@gmail.com>
2025-07-08 21:21:37 +0100

    feat: 002

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import Home from "./pages/Home.vue";
 </script>
 
 <template>
-  <Home />
+  <router-view />
 </template>
 
 <style scoped>
@@ -13,11 +13,5 @@ import Home from "./pages/Home.vue";
   padding: 1.5em;
   will-change: filter;
   transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
 }
 </style>
