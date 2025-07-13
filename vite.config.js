0b08509042ccaafdb716780995b436e69ca7feed
inno-cent <ekweozorinnocent@gmail.com>
2025-07-13 23:44:02 +0100

    feat: 002

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import tailwindcss from "@tailwindcss/vite";
 import vue from "@vitejs/plugin-vue";
 
 // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [vue(), tailwindcss()],
-// });
+export default defineConfig({
+  plugins: [vue(), tailwindcss()],
+});
